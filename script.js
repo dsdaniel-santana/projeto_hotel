f4a20415eba2dd76cfcf614e9fd4310396b88721
Daniel Santana <daniel_santana_87@hotmail.com>
2024-02-06 22:17:23 -0300

    criando arquivos e class

---
+++ script.js
@@ -0,0 +1,9 @@
+class Quarto {
+    constructor (numero, tipo, precoDiaria, reservado){
+        this.numero = numero
+        this.tipo = tipo
+        this.precoDiaria = precoDiaria
+        this.reservado = reservado
+
+    }
+}
